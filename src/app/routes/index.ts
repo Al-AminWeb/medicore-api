c22203bb4feabd0a92ba04af5f8d0612da751c91
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-17 00:22:49 +0600

    feat: integrate `better-auth` for authentication and add user, session, and account models in Prisma schema

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,12 @@
 import {Router} from "express";
 import {specialityRoute} from "../module/specialty/speciality.route";
+import {authRoutes} from "../module/auth/auth.routes";
 
 
 const router = Router();
 
 router.use("/specialities", specialityRoute)
+router.use('/auth',authRoutes)
+
 
 export const indexRoute = router;
