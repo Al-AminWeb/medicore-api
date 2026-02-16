2101909091f4191ee28f089d1e46009c72ad93d5
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-16 20:37:06 +0600

    refactor: centralize response handling with `sendResponse` utility in specialty controller

--- src/app/module/specialty/speciality.controller.ts
+++ src/app/module/specialty/speciality.controller.ts
@@ -1,55 +1,59 @@
 import {specialityService} from "./speciality.service";
 import {Request, Response} from "express";
 import {catchAsync} from "../../shared/catchAsync";
+import {sendResponse} from "../../shared/sendResponse";
 
 
 const createSpeciality = catchAsync(
     async (req: Request, res: Response) => {
         const payload = req.body;
         const result = await specialityService.createSpeciality(payload);
-        res.status(201).json({
+        sendResponse(res, {
+            httpStatusCode: 201,
             success: true,
             message: "Speciality created successfully",
-            data: result
-        });
-    }
-)
+            data: result,
+        })
+    });
 
 
 const getAllSpeciality = catchAsync(
     async (req: Request, res: Response) => {
         const specialities = await specialityService.getAllSpeciality();
-        res.status(200).json({
+        sendResponse(res, {
+            httpStatusCode: 201,
             success: true,
+            message: "Speciality fetched successfully",
             data: specialities
         })
-    }
-)
+    })
+
 
 const deleteSpeciality = catchAsync(
     async (req: Request, res: Response) => {
         const {id} = req.params;
         const result = await specialityService.deleteSpeciality(id as string);
-        res.status(200).json({
+        sendResponse(res, {
+            httpStatusCode: 200,
             success: true,
             message: "Speciality deleted successfully",
             data: result
         })
-    }
-)
+    })
+
 
 const updateSpeciality = catchAsync(
     async (req: Request, res: Response) => {
         const {id} = req.params;
         const payload = req.body;
         const result = await specialityService.updateSpeciality(id as string, payload);
-        res.status(200).json({
+        sendResponse(res,{
+            httpStatusCode: 200,
             success: true,
             message: "Speciality updated successfully",
             data: result
         })
-    }
-)
+    })
 
 export const specialityController = {
     createSpeciality,
