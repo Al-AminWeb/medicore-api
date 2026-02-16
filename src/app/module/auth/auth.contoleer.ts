ccfe2e3d7738c40b326cb99909118730b6bbc432
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-17 00:46:03 +0600

    feat: add patient registration functionality to auth module, including routes, controller, and service

---
+++ src/app/module/auth/auth.contoleer.ts
@@ -0,0 +1,23 @@
+ import {catchAsync} from "../../shared/catchAsync";
+import {authService} from "./auth.service";
+import {Request, Response} from "express";
+import {sendResponse} from "../../shared/sendResponse";
+
+
+const registerPatient = catchAsync(
+    async (req:Request, res:Response) => {
+        const payload = req.body;
+        const result = await authService.registerPatient(payload)
+        sendResponse(res,{
+            httpStatusCode: 201,
+            success: true,
+            message: "Patient registered successfully",
+            data: result
+        })
+    }
+)
+
+
+ export const authController = {
+     registerPatient
+ }
--- src/app/module/auth/auth.routes.ts
+++ src/app/module/auth/auth.routes.ts
@@ -1,7 +1,9 @@
 import {Router} from "express";
+import {authController} from "./auth.contoleer";
 
 
 const router = Router();
 
+router.post('/register',authController.registerPatient)
 
 export const authRoutes = router;
