20e48eadc938dbd017d7ea31f9e30a5fc918cf85
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-23 03:25:19 +0600

    feat: implement email sending functionality with nodemailer and ejs templates

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -5,6 +5,12 @@ import AppError from "../errorHelper/AppError";
 
 dotenv.config();
 
+
+//EMAIL_SENDER_SMTP_USER=mdalaminweb.1@gmail.com
+// EMAIL_SENDER_SMTP_PASS=vzav sujz wrvg hfho
+// EMAIL_SENDER_SMTP_HOST=smtp.gmail.com
+// EMAIL_SENDER_SMTP_PORT=465
+// EMAIL_SENDER_SMTP_FROM=mdalaminweb.1@gmail.com
 interface EnvConfig {
     NODE_ENV: string;
     PORT: string;
@@ -17,6 +23,13 @@ interface EnvConfig {
     REFRESH_TOKEN_EXPIRES_IN: string;
     BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: string;
     BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: string;
+    EMAIL_SENDER:{
+        SMTP_USER:string,
+        SMTP_PASS:string,
+        SMTP_HOST:string,
+        SMTP_PORT:string,
+        SMTP_FROM:string
+    }
 }
 
 
@@ -33,7 +46,13 @@ const loadEnvVariables = (): EnvConfig => {
         'ACCESS_TOKEN_EXPIRES_IN',
         'REFRESH_TOKEN_EXPIRES_IN',
         'BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN',
-        'BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE'
+        'BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE',
+        'EMAIL_SENDER_SMTP_USER',
+        'EMAIL_SENDER_SMTP_PASS',
+        'EMAIL_SENDER_SMTP_HOST',
+        'EMAIL_SENDER_SMTP_PORT',
+        'EMAIL_SENDER_SMTP_FROM'
+
     ]
 
     requireEnvVariable.forEach((variable) => {
@@ -55,6 +74,13 @@ const loadEnvVariables = (): EnvConfig => {
         REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
         BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: process.env.BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
         BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: process.env.BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE as string,
+        EMAIL_SENDER:{
+            SMTP_USER:process.env.EMAIL_SENDER_SMTP_USER as string,
+            SMTP_PASS:process.env.EMAIL_SENDER_SMTP_PASS as string,
+            SMTP_HOST:process.env.EMAIL_SENDER_SMTP_HOST as string,
+            SMTP_PORT:process.env.EMAIL_SENDER_SMTP_PORT as string,
+            SMTP_FROM:process.env.EMAIL_SENDER_SMTP_FROM as string
+        }
     }
 }
 
