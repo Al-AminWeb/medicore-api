7d9ef960fb8409458dffe3599e39570abd4cbd72
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-26 00:01:45 +0600

    feat: integrate Cloudinary and Multer for file uploading
    - Added Cloudinary configuration for media uploads with secure credentials.
    - Implemented Multer with Cloudinary storage for handling image and PDF uploads.
    - Updated `.env` file to include Cloudinary credentials.
    - Installed necessary dependencies: `cloudinary`, `multer`, `multer-storage-cloudinary`, and their types.

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,11 +6,9 @@ import AppError from "../errorHelper/AppError";
 dotenv.config();
 
 
-//EMAIL_SENDER_SMTP_USER=mdalaminweb.1@gmail.com
-// EMAIL_SENDER_SMTP_PASS=vzav sujz wrvg hfho
-// EMAIL_SENDER_SMTP_HOST=smtp.gmail.com
-// EMAIL_SENDER_SMTP_PORT=465
-// EMAIL_SENDER_SMTP_FROM=mdalaminweb.1@gmail.com
+// CLOUDINARY_CLOUD_NAME=dpgjdfxc7
+// CLOUDINARY_API_KEY=857424352467291
+// CLOUDINARY_API_SECRET=nYRScfxno4LwTtmsycU1o40SQeE
 interface EnvConfig {
     NODE_ENV: string;
     PORT: string;
@@ -34,6 +32,12 @@ interface EnvConfig {
     GOOGLE_CLIENT_SECRET: string,
     GOOGLE_CALLBACK_URL: string,
     FRONTEND_URL: string,
+    CLOUDINARY:{
+        CLOUDINARY_CLOUD_NAME: string,
+        CLOUDINARY_API_KEY: string,
+        CLOUDINARY_API_SECRET: string,
+    }
+
 }
 
 
@@ -59,7 +63,10 @@ const loadEnvVariables = (): EnvConfig => {
         'GOOGLE_CLIENT_ID',
         'GOOGLE_CLIENT_SECRET',
         'GOOGLE_CALLBACK_URL',
-        'FRONTEND_URL'
+        'FRONTEND_URL',
+        'CLOUDINARY_CLOUD_NAME',
+        'CLOUDINARY_API_KEY',
+        'CLOUDINARY_API_SECRET',
 
     ]
 
@@ -93,6 +100,12 @@ const loadEnvVariables = (): EnvConfig => {
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
         GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
         FRONTEND_URL: process.env.FRONTEND_URL as string,
+        CLOUDINARY:{
+            CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+            CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+            CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+        }
+
     }
 }
 
