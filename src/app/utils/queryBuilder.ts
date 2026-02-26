15a09baaaec0c9e98fad80e95043b11baf689ef1
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-02-27 03:34:52 +0600

    feat: add query interfaces and builder for Prisma integration

---
+++ src/app/utils/queryBuilder.ts
@@ -0,0 +1,45 @@
+import {
+    IQueryConfig,
+    IQueryParams,
+    PrismaCountArgs,
+    PrismaFindManyArgs,
+    PrismaModelDelegate
+} from "../interfaces/query.interface";
+
+export class QueryBuilder<
+    T,
+    TWhereInput = Record<string, unknown>,
+    TInclude = Record<string, unknown>
+
+> {
+    // @ts-ignore
+    private query: PrismaFindManyArgs;
+    // @ts-ignore
+    private countQuery: PrismaCountArgs;
+    private page: number = 1;
+    private limit: number = 10;
+    private skip: number = 0;
+    private sportBy: string = "createdAt";
+    private sortOrder: "asc" | "desc" = "asc";
+    // @ts-ignore
+    private selectFields: Record<string, boolean | undefined>
+
+
+    constructor(
+        private model: PrismaModelDelegate,
+        private queryParams: IQueryParams,
+        private config: IQueryConfig
+    ) {
+        this.query = {
+            where: {},
+            include: {},
+            orderBy: {},
+            skip: 0,
+            take: 10,
+        },
+            this.countQuery = {
+                where: {},
+
+            }
+    }
+}
