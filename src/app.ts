891edc4a6c0c387b2f4461ed29673cd34ce6eb6d
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-03-03 01:55:03 +0600

    feat: implement Stripe webhook handling for payment processing

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import {toNodeHandler} from "better-auth/node";
 import {auth} from "./app/lib/auth";
 import path from "path";
 import qs from "qs";
+import {PaymentController} from "./app/module/payment/payment.controller";
 
 
 dotenv.config();
@@ -20,13 +21,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.resolve(process.cwd(), `src/app/templates/`));
 
 
-app.post("/webhook", express.raw({ type: "application/json" }),
-    async(req: Request, res: Response) => {
-        console.log('webhook received:',req.body)
-        res.status(200).send('ok')
-    }
-
-    )
+app.post("/webhook", express.raw({ type: "application/json" }),PaymentController.handleStripeWebhookEvent)
 
 
 
