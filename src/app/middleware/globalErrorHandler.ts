c269aa6ec2882e8dcf8260dd56691afc075fe22e
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-03-06 04:17:42 +0600

    feat: implement global error handler for deleting uploaded files from Cloudinary

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -6,6 +6,7 @@ import z from "zod";
 import {handleZodError} from "../errorHelper/handleZodError";
 import AppError from "../errorHelper/AppError";
 import {deleteFileFromCloudinary} from "../config/cloudinary.config";
+import {deleteUploadedFilesFromGlobalErrorHandler} from "../utils/deleteUploadFIleFromGlobalErrorHandler";
 
 export const globalErrorHandler = async (err: any, req: Request, res: Response, next: NextFunction) => {
 
@@ -22,14 +23,16 @@ export const globalErrorHandler = async (err: any, req: Request, res: Response, 
     if (envVars.NODE_ENV === "development") {
         console.log("Full Error:", err);
     }
-    if(req.file){
-        await deleteFileFromCloudinary(req.file.path)
-    }
+    // if(req.file){
+    //     await deleteFileFromCloudinary(req.file.path)
+    // }
+    //
+    // if(req.files && Array.isArray(req.files) && req.files.length > 0){
+    //     const imageUrls = req.files.map((file) => file.path);
+    //     await Promise.all(imageUrls.map(url => deleteFileFromCloudinary(url)));
+    // }
 
-    if(req.files && Array.isArray(req.files) && req.files.length > 0){
-        const imageUrls = req.files.map((file) => file.path);
-        await Promise.all(imageUrls.map(url => deleteFileFromCloudinary(url)));
-    }
+    await deleteUploadedFilesFromGlobalErrorHandler(req);
 
 
     // FIX: Initialize as array, not single object
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,16 +6,16 @@ import {doctorRoute} from "../module/doctor/doctor.route";
 import {adminRoutes} from "../module/admin/admin.route";
 import { scheduleRoutes } from "../module/schedule/schedule.route";
 import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
+import {PatientRoutes} from "../module/patient/patient.route";
 
 
 const router = Router();
-
 router.use("/specialities", specialityRoute)
 router.use('/auth',authRoutes)
 router.use("/users",userRoute)
+router.use("/patients",PatientRoutes)
 router.use("/doctors",doctorRoute)
 router.use("/admins", adminRoutes)
-
 router.use("/schedules", scheduleRoutes)
 router.use("/doctor-schedules", DoctorScheduleRoutes)
 // router.use("/appointments", AppointmentRoutes)
