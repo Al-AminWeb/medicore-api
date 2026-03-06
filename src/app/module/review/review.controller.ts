beb5d4c64f88bbdee755668791710824e6448f0b
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-03-07 00:31:07 +0600

    feat: add functionality for submitting reviews with validations and rating updates

--- src/app/module/review/review.controller.ts
+++ src/app/module/review/review.controller.ts
@@ -1,4 +1,22 @@
+import {catchAsync} from "../../shared/catchAsync";
+import {Request, Response} from "express";
+import {sendResponse} from "../../shared/sendResponse";
+import {reviewService} from "./review.service";
+import httpStatus from "http-status";
 
+const giveReview = catchAsync(
+    async (req: Request, res: Response) => {
+        const payload = req.body;
+        const user = req.user;
+        const result = await reviewService.giveReview(user, payload);
+        sendResponse(res, {
+            httpStatusCode: httpStatus.OK,
+            success: true,
+            message: "Review given successfully",
+            data: result
+        })
+    }
+)
 
 export const ReviewController = {
     getAllReviews,
--- src/app/module/review/review.service.ts
+++ src/app/module/review/review.service.ts
@@ -1,19 +1,87 @@
 import {IRequestUser} from "../../interfaces/requestUser.Interfaces";
 import {ICreateReviewPayload} from "./review.interface";
 import {prisma} from "../../lib/prisma";
+import {PaymentStatus} from "../../../generated/prisma/enums";
+import status from "http-status";
+import AppError from "../../errorHelper/AppError";
 
 const giveReview = async (user: IRequestUser, payload: ICreateReviewPayload) => {
-    const patient = await prisma.patient.findUnique({
+    const patient = await prisma.patient.findUniqueOrThrow({
         where: {
             email: user.email
         }
     })
+
+    const appointmentData = await prisma.appointment.findUniqueOrThrow({
+        where: {
+            id: payload.appointmentId
+        }
+    })
+
+    if (appointmentData.paymentStatus !== PaymentStatus.PAID) {
+        // @ts-ignore
+        throw new AppError(status.BAD_REQUEST, "You cannot give review for unpaid appointment");
+    }
+
+    if (appointmentData.patientId !== patient.id) {
+        // @ts-ignore
+        throw new AppError(status.BAD_FORBIDDEN, "You cannot give review for other patient's appointment");
+    }
+
+    const isReviewed = await prisma.review.findFirst({
+        where: {
+            appointmentId: payload.appointmentId
+        }
+    })
+
+    if (isReviewed) {
+        throw new AppError(status.BAD_REQUEST, "You have already given review for this appointment")
+    }
+    const result = await prisma.$transaction(async (tx) => {
+        const review = await tx.review.create({
+            data: {
+                ...payload,
+                patientId: appointmentData.patientId,
+                doctorId: appointmentData.doctorId,
+            }
+        })
+        const averageRating = await tx.review.aggregate({
+            where: {
+                doctorId: appointmentData.doctorId
+            },
+
+            _avg: {
+                rating: true,
+            }
+        })
+
+        await tx.doctor.update({
+            where:{
+                id:appointmentData.doctorId
+            },
+            data:{
+                averageRating: averageRating._avg.rating as number,
+            }
+        });
+        return review;
+    });
+
+    return result;
 };
 
 const getAllReviews = async () => {
+    const reviews = await prisma.review.findMany({
+        include: {
+            patient: true,
+            doctor: true,
+            appointment:true,
+        }
+    })
+    return reviews;
 };
 
-const myReviews = async () => {
+const myReviews = async (user:IRequestUser) => {
+
 };
 
 const updateReview = async () => {
