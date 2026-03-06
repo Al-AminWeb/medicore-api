cc75f78e25d8ac2f726a484177f0c8215e02749e
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-03-07 02:31:26 +0600

    feat: add CRUD functionality for reviews with role-based access and validations

--- src/app/module/review/review.controller.ts
+++ src/app/module/review/review.controller.ts
@@ -18,6 +18,57 @@ const giveReview = catchAsync(
     }
 )
 
+const getAllReviews = catchAsync(
+    async (req: Request, res: Response) => {
+        const result = await reviewService.getAllReviews();
+        sendResponse(res, {
+            httpStatusCode: httpStatus.OK,
+            success: true,
+            message: "Reviews fetched successfully",
+            data: result
+        })
+    }
+)
+
+const myReviews = catchAsync(
+    async (req: Request, res: Response) => {
+        const user = req.user;
+        const result = await reviewService.myReviews(user);
+        sendResponse(res, {
+            httpStatusCode: httpStatus.OK,
+            success: true,
+            message: "Reviews retrieval successfully",
+        })
+    })
+
+
+const updateReview = catchAsync(async (req: Request, res: Response) => {
+        const user = req.user;
+        const reviewId = req.params.id;
+        const payload = req.body;
+
+        const result = await reviewService.updateReview(user, reviewId as string, payload);
+        sendResponse(res, {
+            httpStatusCode: httpStatus.OK,
+            success: true,
+            message: 'Review updated successfully',
+            data: result
+        });
+    }
+);
+
+const deleteReview = catchAsync(async (req: Request, res: Response) => {
+    const user = req.user;
+    const reviewId = req.params.id;
+    const result = await reviewService.deleteReview(user, reviewId as string);
+    sendResponse(res, {
+        httpStatusCode: httpStatus.OK,
+        success: true,
+        message: 'Review deleted successfully',
+        data: result
+    });
+});
+
 export const ReviewController = {
     getAllReviews,
     giveReview,
--- src/app/module/review/review.service.ts
+++ src/app/module/review/review.service.ts
@@ -1,7 +1,7 @@
 import {IRequestUser} from "../../interfaces/requestUser.Interfaces";
-import {ICreateReviewPayload} from "./review.interface";
+import {ICreateReviewPayload, IUpdateReviewPayload} from "./review.interface";
 import {prisma} from "../../lib/prisma";
-import {PaymentStatus} from "../../../generated/prisma/enums";
+import {PaymentStatus, Role} from "../../../generated/prisma/enums";
 import status from "http-status";
 import AppError from "../../errorHelper/AppError";
 
@@ -56,10 +56,10 @@ const giveReview = async (user: IRequestUser, payload: ICreateReviewPayload) => 
         })
 
         await tx.doctor.update({
-            where:{
-                id:appointmentData.doctorId
+            where: {
+                id: appointmentData.doctorId
             },
-            data:{
+            data: {
                 averageRating: averageRating._avg.rating as number,
             }
         });
@@ -74,21 +74,148 @@ const getAllReviews = async () => {
         include: {
             patient: true,
             doctor: true,
-            appointment:true,
+            appointment: true,
         }
     })
     return reviews;
 };
 
-const myReviews = async (user:IRequestUser) => {
+const myReviews = async (user: IRequestUser) => {
+    const isUserExist = await prisma.user.findUnique({
+        where: {
+            email: user?.email
+        }
+    })
+    if (!isUserExist) {
+        throw new AppError(status.BAD_REQUEST, "Only patients can view their reviews");
+    }
 
+    if (isUserExist.role === Role.DOCTOR) {
+        const doctorData = await prisma.doctor.findUniqueOrThrow({
+            where: {
+                email: user?.email
+            }
+        });
+        return await prisma.review.findMany({
+            where: {
+                doctorId: doctorData.id
+            },
+            include: {
+                patient: true,
+                appointment: true,
+            }
+        })
+    }
+    if (isUserExist.role === Role.PATIENT) {
+        const patientData = await prisma.patient.findUniqueOrThrow({
+            where: {
+                email: user?.email
+            }
+        });
+        return await prisma.review.findMany({
+            where: {
+                patientId: patientData.id
+            },
+            include: {
+                doctor: true,
+                appointment: true,
+            }
+        })
+    }
 };
 
-const updateReview = async () => {
+const updateReview = async (user: IRequestUser, reviewId: string, payload: IUpdateReviewPayload) => {
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where: {
+            email: user.email
+        }
+    });
+    const reviewData = await prisma.review.findUniqueOrThrow({
+        where: {
+            id: reviewId
+        }
+    });
+
+    if (!(patientData.id === reviewData.patientId)){
+        throw new AppError(status.BAD_REQUEST, "You cannot update review of other patient");
+    }
+
+    const result = await prisma.$transaction(async (tx) => {
+        const updatedReview = await tx.review.update({
+            where: {
+                id: reviewId
+            },
+            data: {
+                // rating:payload.rating,
+                //  comment:payload.comment,
+                ...payload
+            }
+        });
+        const averageRating = await tx.review.aggregate({
+            where: {
+                doctorId: reviewData.doctorId
+            },
+            _avg: {
+                rating: true
+            }
+        })
+        await tx.doctor.update({
+            where: {
+                id: updatedReview.doctorId
+            },
+            data: {
+                averageRating: averageRating._avg.rating as number
+            }
+        })
+        return updatedReview;
+    })
+
+    return result;
 };
 
 
-const deleteReview = async () => {
+const deleteReview = async (user:IRequestUser,reviewId:string) => {
+
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where: {
+            email: user?.email
+        }
+    });
+    const reviewData = await prisma.review.findUniqueOrThrow({
+        where: {
+            id: reviewId
+        }
+    });
+
+    if (!(patientData.id === reviewData.patientId)){
+       throw new AppError(status.BAD_REQUEST, "You cannot delete review of other patient");
+    }
+
+    const result = await prisma.$transaction(async (tx) => {
+        const deleteReview = await tx.review.delete({
+            where: {
+                id: reviewId
+            }
+        })
+        const averageRating = await tx.review.aggregate({
+            where:{
+                doctorId:deleteReview.doctorId
+            },
+            _avg:{
+                rating:true,
+            }
+        });
+        await tx.doctor.update({
+            where:{
+                id:deleteReview.doctorId
+            },
+            data:{
+                averageRating:averageRating._avg.rating as number
+            }
+        })
+        return deleteReview;
+    })
+    return result;
 
 }
 
@@ -97,5 +224,6 @@ export const reviewService = {
     giveReview,
     getAllReviews,
     myReviews,
-    updateReview
+    updateReview,
+    deleteReview,
 };
